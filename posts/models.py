60ec27af77608208022e622d3b4490f2fe084e87
Terlale <109313642+Terlale@users.noreply.github.com>
2023-01-28 23:26:21 +0400

    Add files via upload

---
+++ posts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from  taggit.managers import TaggableManager
+
+User = get_user_model()
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    subtitle = models.CharField(max_length=20)
+    slug = models.SlugField()
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+
